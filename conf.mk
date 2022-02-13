c9c17f926859324fc100665a6be071f6377ec8d9
clovella <clovella@student.21-school.ru>
2022-02-13 20:07:06 +0300

    use ar deterministic mode

--- conf.mk
+++ conf.mk
@@ -1,12 +1,12 @@
 CC := clang
 CFLAGS := -Wall -Wextra -Werror -O2 -MMD -I include
-ARFLAGS := rs
+ARFLAGS := rsD
 RM := rm -rf
 
-OS := $(shell uname -s)
-ifeq ($(OS),Linux)
-ARFLAGS := rsU
-endif
+# OS := $(shell uname -s)
+# ifeq ($(OS),Linux)
+# ARFLAGS := rsU
+# endif
 MOD_INCLUDE := mod_include.mk
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
 	$(CC) $(CFLAGS) -c -MF $(DEP_DIR)/$*.d $< -o $@
