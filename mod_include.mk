ff8373e4acf649303208b798ab77d5f9b32212f9
clovella <clovella@student.21-school.ru>
2022-02-13 20:26:59 +0300

    Support dependencies between modules

--- mod_include.mk
+++ mod_include.mk
@@ -1,11 +1,10 @@
-MOD_OBJ:=$(patsubst %.c,$(OBJ_DIR)/$(MOD_NAME)/%.o,$(MOD_SRC))
-OBJ:=$(OBJ) $(MOD_OBJ)
-
+$(MOD_NAME)_OBJ:=$(patsubst %.c,$(OBJ_DIR)/$(MOD_NAME)/%.o,$(MOD_SRC))
+OBJ:=$(OBJ) $($(MOD_NAME)_OBJ)
 $(MOD_NAME): lib$(MOD_NAME).a
 
-lib$(MOD_NAME).a : $(MOD_OBJ)
+lib$(MOD_NAME).a : $(patsubst %,$$(%_OBJ),$(MOD_DEPS) $(MOD_NAME))
 
-$(MOD_OBJ):|$(OBJ_DIR)/$(MOD_NAME) $(DEP_DIR)/$(MOD_NAME)
+$($(MOD_NAME)_OBJ):|$(OBJ_DIR)/$(MOD_NAME) $(DEP_DIR)/$(MOD_NAME)
 
 $(OBJ_DIR)/$(MOD_NAME):
 	mkdir -p $@
