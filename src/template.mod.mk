f413928850f933b887fd392a104f958481c82407
clovella <clovella@student.21-school.ru>
2022-02-13 20:27:27 +0300

    mod.mk template

---
+++ src/template.mod.mk
@@ -0,0 +1,4 @@
+MOD_DEPS:= #Dependencies of this module
+MOD_NAME:= #Module name
+MOD_SRC	:= #This module's C files
+include $(MOD_INCLUDE) #Common logic for all modules based on the 3 variables above
