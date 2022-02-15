1578e76c94e3836d24273ebe831f8fe8857d3ec3
clovella <clovella@student.21-school.ru>
2022-02-15 21:01:34 +0300

    ft_str_join: t_str "from" now passing by value

--- include/libft_str.h
+++ include/libft_str.h
@@ -6,7 +6,7 @@
 /*   By: clovella <clovella@student.school-21.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 19:02:20 by clovella          #+#    #+#             */
-/*   Updated: 2022/02/15 20:42:11 by clovella         ###   ########.fr       */
+/*   Updated: 2022/02/15 20:55:37 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,5 @@ t_str	ft_str_with_cap(size_t cap);
 int		ft_str_trim(t_str *str);
 int		ft_str_overcap(t_str *str, size_t overcap);
 int		ft_str_append(t_str *str, char *apnd, size_t apnd_size);
-int		ft_str_join(t_str *to, t_str *from);
+int		ft_str_join(t_str *to, t_str from);
 #endif
--- src/ft_str/ft_str_join.c
+++ src/ft_str/ft_str_join.c
@@ -6,13 +6,13 @@
 /*   By: clovella <clovella@student.school-21.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 20:42:34 by clovella          #+#    #+#             */
-/*   Updated: 2022/02/15 20:49:48 by clovella         ###   ########.fr       */
+/*   Updated: 2022/02/15 20:55:22 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_str.h"
 
-int	ft_str_join(t_str *to, t_str *from)
+int	ft_str_join(t_str *to, t_str from)
 {
-	return (ft_str_append(to, from->data, from->len));
+	return (ft_str_append(to, from.data, from.len));
 }
