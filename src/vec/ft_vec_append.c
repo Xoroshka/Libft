ea75e5dd8bdb5a0c7e9f2b01dc370fcdefcd0123
clovella <clovella@student.21-school.ru>
2022-10-07 06:34:03 +0300

    get vec by value when it possible

--- src/vec/ft_vec_append.c
+++ src/vec/ft_vec_append.c
@@ -6,7 +6,7 @@
 /*   By: clovella <clovella@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 22:30:35 by clovella          #+#    #+#             */
-/*   Updated: 2022/09/29 04:21:25 by clovella         ###   ########.fr       */
+/*   Updated: 2022/10/07 06:23:27 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 #include "libft_vec.h"
 #include "libft_std.h"
 
-t_res	ft_vec_append(t_vec *to, const t_vec *from)
+t_res	ft_vec_append(t_vec *to, t_vec from)
 {
-	if (to->elm_size != from->elm_size)
+	if (to->elm_size != from.elm_size)
 		return (err);
-	if (ft_vec_reserve(to, from->len) == err)
+	if (ft_vec_reserve(to, from.len) == err)
 		return (err);
 	ft_memcpy(to->data + (to->len * to->elm_size),
-		from->data, from->len * from->elm_size);
-	to->len += from->len;
+		from.data, from.len * from.elm_size);
+	to->len += from.len;
 	return (ok);
 }
--- src/vec/ft_vec_elm.c
+++ src/vec/ft_vec_elm.c
@@ -6,15 +6,15 @@
 /*   By: clovella <clovella@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 10:01:17 by clovella          #+#    #+#             */
-/*   Updated: 2022/04/29 10:33:45 by clovella         ###   ########.fr       */
+/*   Updated: 2022/10/07 06:23:46 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_types.h"
 
-void	*ft_vec_elm(t_vec *vec, t_u64 index)
+void	*ft_vec_elm(t_vec vec, t_u64 index)
 {
-	if (index >= vec->len)
+	if (index >= vec.len)
 		return ((void *)err);
-	return (vec->data + index * vec->elm_size);
+	return (vec.data + index * vec.elm_size);
 }
