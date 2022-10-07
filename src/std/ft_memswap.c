80c8ac783b7a0b2f866b8b2106aa8faec623b307
clovella <clovella@student.21-school.ru>
2022-10-07 05:58:19 +0300

    improve memswap

--- src/std/ft_memswap.c
+++ src/std/ft_memswap.c
@@ -6,24 +6,28 @@
 /*   By: clovella <clovella@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 19:56:54 by clovella          #+#    #+#             */
-/*   Updated: 2022/09/29 05:29:20 by clovella         ###   ########.fr       */
+/*   Updated: 2022/10/07 05:55:59 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_types.h"
 
-void	ft_memswap(void *restrict ptr1, void *restrict ptr2, t_u64 size)
+void	ft_memswap(void *restrict ptr1, void *restrict ptr2, const t_u64 size)
 {
-	char	tmp;
-	char	*p1;
-	char	*p2;
+	unsigned char	tmp;
+	unsigned char	*p1;
+	unsigned char	*p2;
+	unsigned char	*p1_end;
 
 	p1 = ptr1;
 	p2 = ptr2;
-	while (size--)
+	p1_end = p1 + size;
+	while (p1 < p1_end)
 	{
-		tmp = p1[size];
-		p1[size] = p2[size];
-		p2[size] = tmp;
+		tmp = *p1;
+		*p1 = *p2;
+		*p2 = tmp;
+		p1++;
+		p2++;
 	}
 }
