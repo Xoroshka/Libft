e1cf4715c4baa9728213204a3d405242a6b818ed
clovella <clovella@student.21-school.ru>
2022-10-10 01:20:55 +0300

    replace t_bool with t_res in fallable functions

--- include/libft_str.h
+++ include/libft_str.h
@@ -6,7 +6,7 @@
 /*   By: clovella <clovella@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 19:02:20 by clovella          #+#    #+#             */
-/*   Updated: 2022/04/29 13:39:26 by clovella         ###   ########.fr       */
+/*   Updated: 2022/10/08 08:03:47 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 # include <stddef.h>
 # include "libft_types.h"
 
-t_bool	ft_str_append(t_str *to, t_str from);
-t_bool	ft_str_concat(t_str *str, char *cstr, size_t size);
+t_res	ft_str_append(t_str *to, t_str from);
+t_res	ft_str_concat(t_str *str, char *cstr, size_t size);
 t_str	ft_str_new(void);
 t_res	ft_str_push(t_str *str, char c);
 char	*ft_str2cstr(t_str str);
--- src/str/ft_str_append.c
+++ src/str/ft_str_append.c
@@ -6,13 +6,13 @@
 /*   By: clovella <clovella@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 20:42:34 by clovella          #+#    #+#             */
-/*   Updated: 2022/04/29 12:20:46 by clovella         ###   ########.fr       */
+/*   Updated: 2022/10/08 08:04:05 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_str.h"
 
-t_bool	ft_str_append(t_str *to, t_str from)
+t_res	ft_str_append(t_str *to, t_str from)
 {
 	return (ft_str_concat(to, (char *)from.data, from.len));
 }
