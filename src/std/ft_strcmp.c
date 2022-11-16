c2fa48d5d19ce4cee9d4bc2f946af7371d6d061f
clovella <clovella@student.21-school.ru>
2022-11-16 20:23:00 +0300

    fix strcmp

--- src/std/ft_strcmp.c
+++ src/std/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: clovella <clovella@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 07:42:31 by clovella          #+#    #+#             */
-/*   Updated: 2022/10/08 07:47:38 by clovella         ###   ########.fr       */
+/*   Updated: 2022/11/16 20:22:10 by clovella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 		s1++;
 		s2++;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
